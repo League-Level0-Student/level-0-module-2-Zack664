92c5c7c6e89b556cf027cdc65dc2fc12a972b771
Zack664 <74957548+Zack664@users.noreply.github.com>
2021-04-20 16:56:16 -0700

    abt start string conversion rollercoaster!

--- src/_02_methods/_6_slippery_dot/slippery_dot/slippery_dot.pde
+++ src/_02_methods/_6_slippery_dot/slippery_dot/slippery_dot.pde
@@ -3,38 +3,44 @@
 ***/
  
  // 1. Create three integer variables to represent the x, y, and the size of the ellipse
-       
+       int x=50;
+       int y=50; 
+       int radius=50;
+    
     void setup() {
       // 2. Set the size of your sketch
-    
+  size(500,500);
     }
     
     void draw() {
     
       // 3. Set the background color of your sketch
-      
+
+      background(0,1000,0);
       // 4. Draw an ellipse using the variables created at the top of the sketch for the location and size of your ellipse. 
       //    Make sure it fits in the window. Change the variables if it does not.
-    
+    fill(#FF0000);
+    ellipse(x,y,radius,radius);
     
     }
     
     /******** This method gets called automatically when you press the mouse ************/
     void mousePressed() {
       //5. Create an integer variable called distance
-      
+      int distance= getDistanceFromMouse(x,y);
       //6. The getDistanceFromMouse() method below returns a number.
       //   Set the value of your distance variable to the value returned by the getDistance method
       //   You will need to pass the x and y location of your ellipse to this method.
-        
+    
           
       //7.  Use an if statement to check if your distance variable is < the radius of the ellipse
       //    If it is, make a new x AND y for the ellipse, for a new random location on the window
       //    Hint: Use code like this      x = (int) random(width);
-       
-      
+      if(distance<radius){
+      x = (int) random(width);
+    y = (int) random(height);
+  }
     }
-    
     /********  This method gives you the number of pixels between the mouse and the x,y point ***********/
     int getDistanceFromMouse(int x, int y) {
       return (int)Math.sqrt(Math.pow((mouseX-x),2) + Math.pow((mouseY-y),2));
--- src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
+++ src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
@@ -4,25 +4,31 @@ import javax.swing.JOptionPane;
 
 public class StringsCannotAdd {
 
-		// 1) Make a main method that includes all the steps belowâ€¦. 
-		
+		// 1) Make a main method that includes all the steps below¦. 
+	public static void main(String[] args) {
 			// 2) Ask the user to enter number 4 and hold it in the variable fourAsString 
-			
-			// 3) Ask the user to enter number 6 and hold it in the variable sixAsString 
-			
-			// 4) Print the addition of fourAsString and sixAsString using JOptionPane 
-			
+		
+	String fourAsString=JOptionPane.showInputDialog("Enter Number 4");
+	int fourAsInt = Integer.parseInt(fourAsString);
+	// 3) Ask the user to enter number 6 and hold it in the variable sixAsString 
+	String sixAsString=JOptionPane.showInputDialog("Enter Number 6");
+	int sixAsInt = Integer.parseInt(sixAsString);
+	// 4) Print the addition of fourAsString and sixAsString using JOptionPane 
+	//System.out.println(fourAsString+sixAsString+fiveAsString);
+	
 			// Did you notice 46 instead of 4 + 6 = 10?
 			
 			// 5) Ask the user to enter number 5 and hold it in the variable fiveAsString 
-			
-			// 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString 
+	String fiveAsString=JOptionPane.showInputDialog("Enter Number 5");
+	int fiveAsInt = Integer.parseInt(fiveAsString);
+	//System.out.println(fourAsString+sixAsString+fiveAsString);
+	// 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString 
 			
 			//Did you notice 456 instead of 4 + 6 + 5 = 15?
 			
 			// Insert fourAsString inside the parenthesis of Integer.parseInt() and set the method equal to fourAsInt
 			// Hint: int fourAsInt = Integer.parseInt(fourAsString);
-			
+	System.out.println(fourAsInt + sixAsInt + fiveAsInt);
 			// 7) Do the same for sixAsString. 
 			
 			// 8) Print the addition of fourAsInt and sixAsInt using JOptionPane 
@@ -34,4 +40,5 @@ public class StringsCannotAdd {
 			// 10) Print the addition of fourAsInt, sixAsInt, and fiveAsInt using JOptionPane 
 			
 			// Is your value now 15 instead of 465?
+}
 }
